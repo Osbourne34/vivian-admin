8992a894b75dccc91384f849ad41cdbc8b06cc92
Evgeniy Volkov <evolkov1804@gmail.com>
2023-10-03 13:37:19 +0500

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
+import '@mantine/core/styles.css'
+
+import { MantineProvider } from '@mantine/core'
+import { theme } from '@/shared/theme/theme'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider theme={theme}>
+      <Head>
+        <title>Mantine Template</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
 }
