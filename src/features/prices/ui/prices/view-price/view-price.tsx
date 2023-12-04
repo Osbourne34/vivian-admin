d2cd26aaf689e487800646948064f3838931d6c6
Evgeniy <evolkov1804@gmail.com>
2023-12-05 01:14:00 +0500

    show states for preview price

--- src/features/prices/ui/prices/view-price/view-price.tsx
+++ src/features/prices/ui/prices/view-price/view-price.tsx
@@ -48,7 +48,14 @@ export const ViewPrice = (props: ViewPriceProps) => {
             </Title>
             <Stack>
               {data?.data.products.map((product) => (
-                <ProductCard key={product.id} {...product} />
+                <div key={product.id}>
+                  <ProductCard {...product} />
+                  {product.deleted && (
+                    <Badge mt={'xs'} color={'red'}>
+                      УДАЛЕН
+                    </Badge>
+                  )}
+                </div>
               ))}
             </Stack>
           </Grid.Col>
@@ -59,26 +66,38 @@ export const ViewPrice = (props: ViewPriceProps) => {
             </Title>
             <Stack>
               {data?.data.employees.map((employee) => (
-                <Group key={employee.id} justify="space-between" wrap="nowrap">
-                  <Group wrap="nowrap" gap="xs">
-                    <Avatar src={employee.avatar} />
-                    <Stack gap={0}>
-                      <Text size="sm" lineClamp={2}>
-                        {employee.name}
-                      </Text>
-                      <Text size="sm" c="dimmed">
-                        +{employee.phone}
-                      </Text>
-                    </Stack>
+                <div key={employee.id}>
+                  <Group justify="space-between" wrap="nowrap">
+                    <Group wrap="nowrap" gap="xs">
+                      <Avatar src={employee.avatar} />
+                      <Stack gap={0}>
+                        <Text size="sm" lineClamp={2}>
+                          {employee.name}
+                        </Text>
+                        <Text size="sm" c="dimmed">
+                          +{employee.phone}
+                        </Text>
+                      </Stack>
+                    </Group>
+                    <Group gap="xs" justify="flex-end">
+                      {employee.roles.map((role) => (
+                        <Badge key={role} variant="light" color="blue">
+                          {role}
+                        </Badge>
+                      ))}
+                    </Group>
                   </Group>
-                  <Group gap="xs" justify="flex-end">
-                    {employee.roles.map((role) => (
-                      <Badge key={role} variant="light" color="blue">
-                        {role}
-                      </Badge>
-                    ))}
-                  </Group>
-                </Group>
+                  {(employee.states.deleted || employee.states.blocked) && (
+                    <Group gap={'xs'} mt={'xs'}>
+                      {employee.states.deleted && (
+                        <Badge color={'red'}>УДАЛЕН</Badge>
+                      )}
+                      {employee.states.blocked && (
+                        <Badge color={'red'}>ЗАБЛОКИРОВАН</Badge>
+                      )}
+                    </Group>
+                  )}
+                </div>
               ))}
             </Stack>
           </Grid.Col>
